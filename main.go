c38d3bbc481f40fba3c009e67a2a4335d0036e69
Brayden Thistle <bthistle@huronperthcatholic.ca>
2025-11-19 23:30:33 +0000

    added a program to sing the user happy birthday.

---
+++ main.go
@@ -0,0 +1,28 @@
+/*
+* @author Brayden Thistle
+* @version 1.0.0
+* @date 2025-11-19
+* @fileoverfiew this program sings the user happy birthday
+*/
+
+package main
+
+import (
+	"fmt"
+	"strings"
+	"bufio"
+	"os"
+)
+func main () {
+var PersonsName string
+
+reader := bufio.NewReader(os.Stdin)
+
+fmt.Print("What is your name? ")
+PersonsName, _ = reader.ReadString('\n')
+PersonsName = strings.TrimSpace(PersonsName)
+
+fmt.Println("Happy birthday to you. Happy birthday to you. Happy birthday, dear " + PersonsName + ". happy birthday to you.")
+
+fmt.Println("\nDone")
+}
---
+++ main.ts
@@ -0,0 +1,17 @@
+/**
+ * @author Brayden Thistle
+ * @version 1.0.0
+ * @date 2025-11-19
+ * @fileoverview this program sings the user happy birthday
+ */
+
+//variable
+let PersonsName: string;
+
+//input
+PersonsName = String(prompt("What is your name so I can sing you happy birthday?"));
+
+//output
+console.log("Happy birthday to you. Happy birthday to you. Happy birthday, dear " + PersonsName + ". happy birthday to you.");
+
+console.log("\nDone");
